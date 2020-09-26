463ea76f56543ec25ba3d5908d1d5d0ed4585567
Jazmin Esqueda <jazminesqueda@csu.fullerton.edu>
2020-09-25 20:21:19 -0700

    Formatting

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -12,8 +12,6 @@
         throw new Error('Could not find element with selector: ' + selector);
       }
     }
-
-
     FormHandler.prototype.addSubmitHandler = function (fn) {
       console.log('Setting submit handler for form');
       this.$formElement.on('submit', function (event) {
@@ -30,8 +28,6 @@
         this.elements[0].focus();
       });
     };
-
-
     FormHandler.prototype.addInputHandler = function (fn) {
       console.log('Setting input handler for form');
 
@@ -48,11 +44,6 @@
       }
       });
     };
-
-
-    
-  
-  
     App.FormHandler = FormHandler;
     window.App = App;
   
